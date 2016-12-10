b467388caa311e08a2fdf31dc0035f7f099e7e06
Tim Godfrey <timg@commoncode.com.au>
2016-12-10 23:54:01 +1100

    Add hot reloading

--- gifbox-ui/webpack.config.js
+++ gifbox-ui/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ["babel-loader"]
+        loaders: ['react-hot', 'babel-loader']
       },
       {
         test: /\.html$/,
