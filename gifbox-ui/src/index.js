596c51cb013b14d98f3d005dd1bbb1d2f11cc94c
Tim Godfrey <timg@commoncode.com.au>
2016-12-11 23:11:44 +1100

    JS cleanup

--- gifbox-ui/src/index.js
+++ gifbox-ui/src/index.js
@@ -2,28 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+// import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
 import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
 
 import App from './App';
 
 // Needed to bring in roboto font
 import './styles/main.css';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const Main = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+  <MuiThemeProvider muiTheme={ getMuiTheme(darkBaseTheme) }>
     <App />
   </MuiThemeProvider>
 );
 
 ReactDOM.render(
   <Main />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
