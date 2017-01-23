3694b3cb9d46e41f55a10cfa771682511b7949e7
Tim Godfrey <timg@commoncode.com.au>
2017-01-23 17:52:16 +1100

    Pre webpack fix

--- gifbox-ui/src/App.js
+++ gifbox-ui/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox/lib/layout';
+import { AppBar } from 'react-toolbox/lib/app_bar';
 
 import GifItem from './components/GifItem';
 
 
 const App = () => (
-  <div>
-    GifBox UI
-  </div>
+  <Layout>
+    <NavDrawer>Stuff</NavDrawer>
+    <Panel>
+      <AppBar>Stuff</AppBar>
+      <GifItem />
+    </Panel>
+  </Layout>
 );
 
 export default App;
--- gifbox-ui/webpack.config.js
+++ gifbox-ui/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 const uiRootPath = path.resolve(__dirname, 'src');
 const buildPath = path.resolve(__dirname, 'dist');
+
 
 module.exports = {
   context: uiRootPath,
@@ -18,17 +21,17 @@ module.exports = {
   },
 
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json'],
+    extensions: ['', '.scss', '.css', '.js', '.json'],
     modules: [
       path.resolve(uiRootPath),
-      path.resolve('./node_modules'),
+      path.resolve(__dirname, './node_modules'),
     ],
   },
 
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /(\.js|\.jsx)$/,
         exclude: /node_modules/,
         loaders: ['react-hot', 'babel-loader'],
       },
@@ -37,16 +40,23 @@ module.exports = {
         loader: 'file?name=[name].[ext]',
       },
       {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /(\.scss|\.css)$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader',
+        }),
       },
     ],
   },
-  // sassLoader: {
-  //   data: '@import "' + path.resolve(__dirname, 'theme/_theme.scss') + '";',
-  // },
+
+  postcss: [autoprefixer],
+
+  sassLoader: {
+    data: '@import "theme/_config.scss";',
+    includePaths: [uiRootPath],
+  },
+
+  plugins: [
+    new ExtractTextPlugin({ filename: 'bundle.css', allChunks: true }),
+  ],
 };
